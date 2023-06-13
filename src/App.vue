4212b1d828728e60889640c2e74f4b71778201a8
Massimiliano-Novello <massinov5@gmail.com>
2023-06-13 16:31:22 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data () {
+    return {
+      project: []
+    }
+  },
+
+  mounted() {
+    this.getProject();
+  },
+
+  methods: {
+    getProject() {
+      axios.get("http://127.0.0.1:8000/api/projects").then(resp => {
+        this.project = resp.data.results;
+      });
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>I mie progetti</h1>
+
+    <div class="row row-cols-4">
+      <div class="col">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+ @use "./style/general.scss" as *;
+</style>
