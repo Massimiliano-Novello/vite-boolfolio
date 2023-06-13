bf394ff1ee05d4e3eda3f3784c9ae4c7542905a1
Massimiliano-Novello <massinov5@gmail.com>
2023-06-13 16:48:21 +0200

    card stampate in pagina

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script>
 import axios from 'axios';
+import projectCard from './components/projectCard.vue';
+import ProjectCard from './components/projectCard.vue';
 
 export default {
+  components: {
+    projectCard,
+},
   data () {
     return {
-      project: []
+      projects: []
     }
   },
 
@@ -15,7 +20,7 @@ export default {
   methods: {
     getProject() {
       axios.get("http://127.0.0.1:8000/api/projects").then(resp => {
-        this.project = resp.data.results;
+        this.projects = resp.data.results;
       });
     }
   }
@@ -24,11 +29,11 @@ export default {
 
 <template>
   <div class="container">
-    <h1>I mie progetti</h1>
+    <h1 class="text-center">I mie progetti</h1>
 
     <div class="row row-cols-4">
-      <div class="col">
-        
+      <div class="col" v-for="project in projects" :key="project.id">
+        <projectCard  :project="project"/>
       </div>
     </div>
   </div>
---
+++ src/components/projectCard.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    name: 'projectCard',
+    props: {
+        project: Object
+    }
+}
+</script>
+
+
+<template>
+
+<div class="card">
+  <div class="card-body">
+    <h3 class="card-title">{{project.titolo}}</h3>
+  </div>
+</div>
+
+</template>
+
+
+<style lang="scss" scoped>
+</style>
