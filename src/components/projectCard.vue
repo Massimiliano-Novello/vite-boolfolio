ba8a29f60c1ea107c49c4d59bd7ec674c8a6c4b0
Massimiliano-Novello <massinov5@gmail.com>
2023-06-14 16:04:15 +0200

    tabella type stampata sul font end

--- src/components/projectCard.vue
+++ src/components/projectCard.vue
@@ -14,6 +14,10 @@ export default {
 <div class="card h-100">
   <div class="card-body">
     <h3 class="card-title text-center">{{project.titolo}}</h3>
+    <p class="text-primary mt-4">
+      <span v-if="project.type">{{ project.type.nome }}</span>
+      <span v-else>Nessuna tipologia</span>
+    </p>
   </div>
 </div>
 
